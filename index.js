f0ca2e284b55bf648c1f0a18f8ac9eecb3e71660
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-01-18 18:42:34 +0000

    read file with fs.createReadStream

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-import { parse } from 'csv-parse';
+import { parse } from "csv-parse";
+import * as fs from "fs";
+
+const results = [];
+
+fs.createReadStream("kepler_data.csv")
+    .on("data", data => {
+        results.push(data);
+    })
+    .on("error", err => {
+        console.log(err);
+    })
+    .on("end", () => {
+        console.log(results);
+        console.log("done");
+    });
+
+// parse();
