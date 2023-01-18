c63933e913f0e9f290f31ef14e66b62573f44603
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-01-18 18:46:08 +0000

    refactor code

--- index.mjs
+++ index.mjs
@@ -3,16 +3,12 @@ import * as fs from "fs";
 
 const results = [];
 
-fs.createReadStream("kepler_datsa.csv")
-    .on("data", data => {
-        results.push(data);
-    })
-    .on("error", err => {
-        console.log(err);
-    })
+fs.createReadStream("kepler_data.csv")
+    .on("data", data => results.push(data))
+    .on("error", err => console.log(err))
     .on("end", () => {
         console.log(results);
         console.log("done");
     });
 
-// parse();
+parse();
