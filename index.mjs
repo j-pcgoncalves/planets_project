dc624254012891fad0b0d21c464f0536c862106b
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-01-18 18:48:46 +0000

    pipe parse to createReadStream

--- index.mjs
+++ index.mjs
@@ -4,11 +4,10 @@ import * as fs from "fs";
 const results = [];
 
 fs.createReadStream("kepler_data.csv")
+    .pipe(parse())
     .on("data", data => results.push(data))
     .on("error", err => console.log(err))
     .on("end", () => {
         console.log(results);
         console.log("done");
     });
-
-parse();
