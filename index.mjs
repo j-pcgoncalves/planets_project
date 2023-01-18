644cd22e9daa6bc43fe9e11eaed81b96af94faee
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-01-18 18:52:33 +0000

    parse data from kepler_data.csv

--- index.mjs
+++ index.mjs
@@ -4,7 +4,10 @@ import * as fs from "fs";
 const results = [];
 
 fs.createReadStream("kepler_data.csv")
-    .pipe(parse())
+    .pipe(parse({
+        comment: "#",
+        columns: true,
+    }))
     .on("data", data => results.push(data))
     .on("error", err => console.log(err))
     .on("end", () => {
