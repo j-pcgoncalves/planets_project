1530743ab229cbdbab03183e0e98d158a7c31891
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-01-18 19:01:06 +0000

    add insolation flux to isHabitablePlanet()

--- index.mjs
+++ index.mjs
@@ -4,7 +4,8 @@ import * as fs from "fs";
 const habitablePlanets = [];
 
 const isHabitablePlanet = planet => {
-    return planet["koi_disposition"] === "CONFIRMED";
+    return planet["koi_disposition"] === "CONFIRMED"
+        && planet["koi_insol"] > 0.36 && planet["koi_insol"] < 1.11;
 }
 
 fs.createReadStream("kepler_data.csv")
